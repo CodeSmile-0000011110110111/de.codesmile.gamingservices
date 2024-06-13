60e85abcbe5b811662a906e57ae455c70bca4b93
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2024-06-13 13:46:12 +0200

    comment test script

--- Editor/Dummy.cs
+++ Editor/Dummy.cs
@@ -1,9 +1,6 @@
 // Copyright (C) 2021-2024 Steffen Itterheim
 // Refer to included LICENSE file for terms and conditions.
 
-using Unity.Services.Apis;
-using Unity.Services.Apis.CloudSave;
-using Unity.Services.Core.Editor.Environments;
 using UnityEditor;
 using UnityEngine;
 
@@ -18,32 +15,32 @@ namespace CodeSmileEditor.Tests
 			// Debug.Log($"org key: {CloudProjectSettings.organizationKey}");
 			// Debug.Log($"access token: {CloudProjectSettings.accessToken}");
 
-			var apiKey = "cf711530-7244-49b2-9dff-72a0f23ab9ad";
-			var apiSecret = "fOdG3auRV1uL6791EIf9LnA-40q_Ic4d";
+			// var apiKey = "??";
+			// var apiSecret = "??";
 
-			await EnvironmentsApi.Instance.RefreshAsync();
-			var envId = EnvironmentsApi.Instance.ActiveEnvironmentId.Value.ToString();
-			var projectId = CloudProjectSettings.projectId;
-
-			Debug.Log($"project: {projectId} ({CloudProjectSettings.projectName})");
-			Debug.Log($"active environment: {envId} ({EnvironmentsApi.Instance.ActiveEnvironmentName})");
-
-			var trustedClient = ApiService.CreateTrustedClient();
-			trustedClient.SetServiceAccount(apiKey, apiSecret);
-			var signInResponse = await trustedClient.SignInWithServiceAccount(projectId, envId);
-			Debug.Log($"trusted sign-in response: {signInResponse.ErrorText} {signInResponse.Content}");
-			var trustedResponse = await trustedClient.CloudSaveData.SetCustomItem(projectId, "trustedTestCustomItemId",
-				new SetItemBody("theKey", "someValue"));
-			Debug.Log($"trusted - set item response: {trustedResponse.ErrorText} {trustedResponse.Content}");
-
-			var adminClient = ApiService.CreateAdminClient();
-			adminClient.SetServiceAccount(apiKey, apiSecret);
-			var adminResponse = await adminClient.CloudSaveData.SetCustomItem(projectId, envId, "adminTestCustomItemId",
-				new Unity.Services.Apis.Admin.CloudSave.SetItemBody("theKey", "someValue"));
-			Debug.Log($"admin - set item response: {adminResponse.ErrorText} {adminResponse.Content}");
-
-			var configResponse = await adminClient.RemoteConfig.RemoteConfigV1ProjectsProjectIdConfigsGet(projectId);
-			Debug.Log($"admin - config: {configResponse.ErrorText} {configResponse.Content}");
+			// await EnvironmentsApi.Instance.RefreshAsync();
+			// var envId = EnvironmentsApi.Instance.ActiveEnvironmentId.Value.ToString();
+			// var projectId = CloudProjectSettings.projectId;
+			//
+			// Debug.Log($"project: {projectId} ({CloudProjectSettings.projectName})");
+			// Debug.Log($"active environment: {envId} ({EnvironmentsApi.Instance.ActiveEnvironmentName})");
+			//
+			// var trustedClient = ApiService.CreateTrustedClient();
+			// trustedClient.SetServiceAccount(apiKey, apiSecret);
+			// var signInResponse = await trustedClient.SignInWithServiceAccount(projectId, envId);
+			// Debug.Log($"trusted sign-in response: {signInResponse.ErrorText} {signInResponse.Content}");
+			// var trustedResponse = await trustedClient.CloudSaveData.SetCustomItem(projectId, "trustedTestCustomItemId",
+			// 	new SetItemBody("theKey", "someValue"));
+			// Debug.Log($"trusted - set item response: {trustedResponse.ErrorText} {trustedResponse.Content}");
+			//
+			// var adminClient = ApiService.CreateAdminClient();
+			// adminClient.SetServiceAccount(apiKey, apiSecret);
+			// var adminResponse = await adminClient.CloudSaveData.SetCustomItem(projectId, envId, "adminTestCustomItemId",
+			// 	new Unity.Services.Apis.Admin.CloudSave.SetItemBody("theKey", "someValue"));
+			// Debug.Log($"admin - set item response: {adminResponse.ErrorText} {adminResponse.Content}");
+			//
+			// var configResponse = await adminClient.RemoteConfig.RemoteConfigV1ProjectsProjectIdConfigsGet(projectId);
+			// Debug.Log($"admin - config: {configResponse.ErrorText} {configResponse.Content}");
 		}
 	}
 }
